6eaa424d6c3dd018706f9dc503ede10c2d32bfde
toastybagel <parker.jay.hanson@gmail.com>
2021-07-16 13:01:23 -0600

    variables created

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,12 @@
-window.alert("This is an alert! JavaScript is running!");
+var playerName = window.prompt("What is your robot's name?");
+console.log(playerName);
+
+var name = "your name";
+console.log(name);
+
+function fight() {
+    window.alert("The fight has begun!");
+  }
+
+// fight();
+
