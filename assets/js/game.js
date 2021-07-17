4b08e0c5929116fd322aafb08a7ce4e01e7971cd
toastybagel <parker.jay.hanson@gmail.com>
2021-07-17 13:51:23 -0600

    multiple rounds complete

--- assets/js/game.js
+++ assets/js/game.js
@@ -80,6 +80,13 @@ window.alert("You need to choose a valid option. Try again!");
     
 
   for (var i = 0; i < enemyNames.length; i++) {
+    if (playerHealth > 0) {
+      window.alert("Welcome to Robot Gladiators! Round " + ( i + 1 ) );
+    }
+    else {
+      window.alert("You have lost your robot in battle! Game Over!");
+      break;
+    }
     var pickedEnemyName = enemyNames[i];
     enemyHealth = 50;
     fight(pickedEnemyName);
