98164268365e9115c975e526848dad022743a535
toastybagel <parker.jay.hanson@gmail.com>
2021-07-17 15:22:20 -0600

    shop function

--- assets/js/game.js
+++ assets/js/game.js
@@ -95,6 +95,10 @@ var startGame = function() {
         enemyHealth = 50;
   
         fight(pickedEnemyName);
+
+        if (playerHealth > 0 && i < enemyNames.length - 1) {
+          shop();
+        }
       }
       else {
         window.alert("You have lost your robot in battle! Game Over!");
@@ -123,6 +127,10 @@ var endGame = function() {
   else {
   window.alert("Thank you for playing Robot Gladiators! Come back soon!");
   }
+};
+
+var shop = function() {
+  console.log("entered the shop");
 };
 
 startGame();
