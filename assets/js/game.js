33be6fd2295910c5e8fbd0aefd2f03c308bc493e
toastybagel <parker.jay.hanson@gmail.com>
2021-07-17 15:43:24 -0600

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -98,6 +98,11 @@ var startGame = function() {
 
         if (playerHealth > 0 && i < enemyNames.length - 1) {
           shop();
+          var storeConfirm = window.confirm("The fight is over, visit the store before the next round?");
+
+          if(storeConfirm) {
+            shop();
+          }
         }
       }
       else {
@@ -130,7 +135,46 @@ var endGame = function() {
 };
 
 var shop = function() {
-  console.log("entered the shop");
+  var shopOptionPrompt = window.prompt(
+    "would you like to REFILL your helath, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice."
+  );
+
+  switch (shopOptionPrompt) {
+    case "REFILL": // new case
+    case "refill":
+      if (playerMoney >= 7) {
+        window.alert("Refilling player's health by 20 for 7 dollars.");
+  
+        playerHealth = playerHealth + 20;
+        playerMoney = playerMoney - 7;
+      }
+      else {
+        window.alert("You don't have enough money!");
+      }
+  
+      break;
+    case "UPGRADE": // new case
+    case "upgrade":
+      if (playerMoney >= 7) {
+        window.alert("Upgrading player's attack by 6 for 7 dollars.");
+  
+        playerAttack = playerAttack + 6;
+        playerMoney = playerMoney - 7;
+      }
+      else {
+        window.alert("You don't have enough money!");
+      }
+  
+      break;
+    case "LEAVE": // new case
+    case "leave":
+      window.alert("Leaving the store.");
+      break;
+    default:
+      window.alert("You did not pick a valid option. Try again.");
+      shop();
+      break;
+  }
 };
 
 startGame();
