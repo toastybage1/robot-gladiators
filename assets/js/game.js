dbf4ec5615907004b84768d852009499adf621ef
toastybagel <parker.jay.hanson@gmail.com>
2021-07-17 17:04:45 -0600

    fixed bug with skipping fights

--- assets/js/game.js
+++ assets/js/game.js
@@ -124,6 +124,38 @@ break;
 }
 // if no (false), ask question again by running fight() again
 else {
+  promptFight = promptFight.toLowerCase();
+    // ask player if they'd like to fight or skip using fightOrSkip function
+    var promptFight = window.prompt('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
+  
+    // Conditional Recursive Function Call
+    if (promptFight === "skip") {
+      if (confirmSkip) {
+        window.alert(playerInfo.name + " has decided to skip this fight. Goodbye!");
+        // subtract money from playerMoney for skipping, but don't let them go into the negative
+        playerInfo.money = Math.max(0, playerInfo.money - 10);
+  
+        // return true if player wants to leave
+        return true;
+      }
+  window.alert("You need to provide a valid answer! Please try again.");
+  return fightOrSkip();
+}
+  
+    // if player picks "skip" confirm and then stop the loop
+    if (promptFight === "skip" || promptFight === "SKIP") {
+      // confirm player wants to skip
+      var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+  
+      // if yes (true), leave fight
+      if (confirmSkip) {
+        window.alert(playerInfo.name + " has decided to skip this fight. Goodbye!");
+        // subtract money from playerMoney for skipping
+        playerInfo.playerMoney = playerInfo.money - 10;
+        shop();
+      }
+    }
+  }
 fight();
 }
 } 
